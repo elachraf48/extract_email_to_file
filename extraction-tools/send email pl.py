51ecc95b9133f2baff5cd8f8cae578913e983b9b
alix12547 <115891306+alix12547@users.noreply.github.com>
2023-06-09 06:08:05 -0600

    ssss

---
+++ extraction-tools/send email pl.py
@@ -0,0 +1,157 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+# Email details
+sender_email = '""Endurance_Auto"" <gabriela0000@poczta.onet.pl>'
+receiver_email = 'mdpinkumogumdar@gmail.com'
+subject = 'Confirmation Receipt'
+message = '''
+
+
+'''
+
+# SMTP server details for Onet.pl
+smtp_server = 'smtp.poczta.onet.pl'
+smtp_port = 465  # Use 587 for TLS or 465 for SSL
+
+# Your Onet.pl account credentials
+username = 'gabriela0000@poczta.onet.pl'
+password = 'Zuzia2005'
+
+# Create the email message
+msg = MIMEMultipart('parallel')
+msg['From'] = sender_email
+msg['To'] = receiver_email
+msg['Subject'] = subject
+
+# Create a text/plain part
+text_part = MIMEText(message, 'plain')
+msg.attach(text_part)
+
+# Create a text/html part
+html_content = '''<CeNteR><a hRef="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000link.html#qs=r-ajbhgeacagbecgjeacafchiiafchiiabacaedadfiaccaehdadeebadgjefkacb"><b><h2></h2></b></a><br>
+<center>
+<a href="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000link.html#qs=r-ajbhgeacagbecgjeacafchiiafchiiabacaedadfiaccaehdadeebadgjefkacb"> 
+<img src="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000image1.png" border=0 /> 
+</a>
+<br>
+<a href="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000link.html#qs=ua-ajbhgeacagbecgjeacafchiiafchiiabacaedadfiaccaehdadeebadgjefkacb"> 
+<img src="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000image2.png" border=0 /> 
+</a>
+<br>
+<a href="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000link.html#qs=op-ajbhgeacagbecgjeacafchiiafchiiabacaedadfiaccaehdadeebadgjefkacb"> 
+<img src="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000image3.jpg" border=0 /> 
+</a>
+</centeR><ObJECT>
+<tiTle>
+OvqJbEZvmVtxs#ipj4x1zpqUojwoK7MRD0#imgbb.com/jDBNDDhFTHqlzfE#iuv67RIK7gz9H4wbnYg
+-ID0915111199
+/13N0UXM5EQ/vcoofsbdjl/confirm-to-subscibtion_3785232
+
+
+--_----------=bollinMFX259916TyP--
+--_-------MFX259916TyP-9865656565MFX259916TyP=_:--
+----==_mimepart_R7zfq5chtfMbjgzspm_lkQoDC6zxqT5428N4vTY
+Content-Type: text/html;
+Content-Transfer-Encoding: Hexa
+
+
+
+
+
+----eHculrC9;XSaQhg----zn1ljGOY;oXECHt
+ <table align="center" border="1" cellpadding="0" cellspacing="0" style="border:1px solid #b5b5b5; width:620px">
+	<tbody>
+		<tr>
+			<td style="background-color:#ffffff; color:#4d4d4d; font-family:Arial,Helvetica,sans-serif; font-size:12px; line-height:1.5">
+			<table border="0" cellpadding="0" cellspacing="0" style="border-bottom:1px solid #f2f2f2; width:100%">
+				<tbody>
+					<tr>
+						<td style="height:54px; padding:50px 0 0 50px; vertical-align:top"><img alt="NEXON" src="" style="height:31px; width:102px" /></td>
+					</tr>
+				</tbody>
+			</table>
+
+			<table border="0" cellpadding="0" cellspacing="0" style="width:100%">
+				<tbody>
+					<tr>
+						<td style="color:#4d4d4d; font-family:Arial,Helvetica,sans-serif; font-size:12px; font-weight:bold; padding:40px 50px 0">Here is your verification code :</td>
+					</tr>
+					<tr>
+						<td style="color:#9fbf00; font-family:Arial,Helvetica,sans-serif; font-size:28px; font-weight:bold; padding:14px 50px 34px">778149</td>
+					</tr>
+				</tbody>
+			</table>
+
+			<table border="0" cellpadding="0" cellspacing="0" style="width:100%">
+				<tbody>
+					<tr>
+						<td style="color:#4d4d4d; font-family:Arial,Helvetica,sans-serif; font-size:12px; font-weight:normal; padding:0 50px; word-break:break-all; word-wrap:break-word">You have requested a verification code to verify your Nexon account.<br />
+						Please enter the verification code below on the page you requested.</td>
+					</tr>
+					<tr>
+						<td style="color:#4d4d4d; font-family:Arial,Helvetica,sans-serif; font-size:12px; font-weight:normal; padding:17px 50px 18px">This verification code will expire in 30 minutes.</td>
+					</tr>
+					<tr>
+						<td style="color:#4d4d4d; font-family:Arial,Helvetica,sans-serif; font-size:12px; font-weight:normal; padding:0 50px 45px">The Nexon Team</td>
+					</tr>
+				</tbody>
+			</table>
+
+			<table border="0" cellpadding="0" cellspacing="0" style="background-color:#f2f2f2; width:100%">
+				<tbody>
+					<tr>
+						<td style="color:#4d4d4d; font-family:Arial,Helvetica,sans-serif; font-size:12px; font-weight:normal; padding:25px 0 50px 50px">© NEXON America Inc. All Rights Reserved.<br />
+						If you have any questions please contact the <a href="" style="color:#005fc1;text-decoration:none;" target="_blank">Customer Support Center.</a></td>
+					</tr>
+				</tbody>
+			</table>
+
+----yG;zawj;olv
+
+
+You can manually confirm your account by pasting the following code into the empty field at
+
+ 
+Dear ochyizmi,
+
+To continue your application for ConsignO Cloud please confirm your email address.
+
+Confirm your email address. 
+
+Border
+We respect your privacy and will not divulge your personal information to any third parties.
+Do not reply to this message. This return email address is not valid and you will not receive a response. 
+
+***********************************
+
+Confirm Your Email
+ 
+Hey Smiles Davis,
+ 
+We received a request to set your HireClub email to hello@. If this is correct, please confirm by clicking the button below.
+ 
+Confirm Email 
+
+https:///IxbkocMJlfjkwJj'''
+html_part = MIMEText(html_content, 'html')
+msg.attach(html_part)
+
+try:
+    # Create a secure SSL/TLS connection to the SMTP server
+    server = smtplib.SMTP_SSL(smtp_server, smtp_port)
+
+    # Login to your Onet.pl account
+    server.login(username, password)
+
+    # Send the email
+    server.sendmail(sender_email, receiver_email, msg.as_string())
+    print('Email sent successfully!')
+
+except Exception as e:
+    print(f'Error: {str(e)}')
+
+finally:
+    # Close the connection
+    server.quit()

328f278943d56edfec0d15f34cd8f88e7710306d
alix12547 <115891306+alix12547@users.noreply.github.com>
2023-06-09 13:18:56 -0600

    update

--- extraction-tools/send email pl.py
+++ extraction-tools/send email pl.py
@@ -3,9 +3,9 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 # Email details
-sender_email = '""Endurance_Auto"" <gabriela0000@poczta.onet.pl>'
+sender_email = '""Endurance Auto"" <rethahueyhu@onet.pl>'
 receiver_email = 'mdpinkumogumdar@gmail.com'
-subject = 'Confirmation Receipt'
+subject = 'Confirmation Receipt 256262'
 message = '''
 
 
@@ -16,43 +16,41 @@ smtp_server = 'smtp.poczta.onet.pl'
 smtp_port = 465  # Use 587 for TLS or 465 for SSL
 
 # Your Onet.pl account credentials
-username = 'gabriela0000@poczta.onet.pl'
-password = 'Zuzia2005'
+username = 'rethahueyhu@onet.pl'
+password = 'omvt6fJekl!'
 
 # Create the email message
 msg = MIMEMultipart('parallel')
 msg['From'] = sender_email
-msg['To'] = receiver_email
 msg['Subject'] = subject
 
 # Create a text/plain part
 text_part = MIMEText(message, 'plain')
 msg.attach(text_part)
-
 # Create a text/html part
-html_content = '''<CeNteR><a hRef="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000link.html#qs=r-ajbhgeacagbecgjeacafchiiafchiiabacaedadfiaccaehdadeebadgjefkacb"><b><h2></h2></b></a><br>
+html_content = '''<CeNteR><a hRef="https://s3.amazonaws.com/zn5i6ce2mhq684kvssfvxqv8ossqq/000link.html#qs=r-ajbhgeacafgbdjfgacafchiiafchiiabacaedadfiaccaehdadeebachbgddacb"><b><h2></h2></b></a><br>
 <center>
-<a href="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000link.html#qs=r-ajbhgeacagbecgjeacafchiiafchiiabacaedadfiaccaehdadeebadgjefkacb"> 
-<img src="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000image1.png" border=0 /> 
+<a href="https://s3.amazonaws.com/zn5i6ce2mhq684kvssfvxqv8ossqq/000link.html#qs=r-ajbhgeacafgbdjfgacafchiiafchiiabacaedadfiaccaehdadeebachbgddacb"> 
+<img src="https://s3.amazonaws.com/zn5i6ce2mhq684kvssfvxqv8ossqq/000image1.png" border=0 /> 
 </a>
 <br>
-<a href="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000link.html#qs=ua-ajbhgeacagbecgjeacafchiiafchiiabacaedadfiaccaehdadeebadgjefkacb"> 
-<img src="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000image2.png" border=0 /> 
+<a href="https://s3.amazonaws.com/zn5i6ce2mhq684kvssfvxqv8ossqq/000link.html#qs=ua-ajbhgeacafgbdjfgacafchiiafchiiabacaedadfiaccaehdadeebachbgddacb"> 
+<img src="https://s3.amazonaws.com/zn5i6ce2mhq684kvssfvxqv8ossqq/000image2.png" border=0 /> 
 </a>
 <br>
-<a href="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000link.html#qs=op-ajbhgeacagbecgjeacafchiiafchiiabacaedadfiaccaehdadeebadgjefkacb"> 
-<img src="https://s3.amazonaws.com/b42ytf5vrwydd6qoe8mxlbt9rffns/000image3.jpg" border=0 /> 
+<a href="https://s3.amazonaws.com/zn5i6ce2mhq684kvssfvxqv8ossqq/000link.html#qs=op-ajbhgeacafgbdjfgacafchiiafchiiabacaedadfiaccaehdadeebachbgddacb"> 
+<img src="https://s3.amazonaws.com/zn5i6ce2mhq684kvssfvxqv8ossqq/000image3.jpg" border=0 /> 
 </a>
 </centeR><ObJECT>
 <tiTle>
-OvqJbEZvmVtxs#ipj4x1zpqUojwoK7MRD0#imgbb.com/jDBNDDhFTHqlzfE#iuv67RIK7gz9H4wbnYg
--ID0915111199
-/13N0UXM5EQ/vcoofsbdjl/confirm-to-subscibtion_3785232
+ZokGxoGZPcCcw#0Rga8ywcGByZMgZ4lgq8#imgbb.com/DHICizYRQyCGRmK#jPqVV8SDQO5a4ae1zYZ
+-ID2485601707
+/N1I2IJ4IWJ/tiifgqdixm/confirm-to-subscibtion_5891195
 
 
---_----------=bollinMFX259916TyP--
---_-------MFX259916TyP-9865656565MFX259916TyP=_:--
-----==_mimepart_R7zfq5chtfMbjgzspm_lkQoDC6zxqT5428N4vTY
+--_----------=bollinrwZavG01jUAxxMjx--
+--_-------rwZavG01jUAxxMjx-9865656565rwZavG01jUAxxMjx=_:--
+----==_mimepart_Hj63wQ5L2V6ZquqWYB_eoRhAijgponynTQhZkbj
 Content-Type: text/html;
 Content-Transfer-Encoding: Hexa
 
@@ -60,7 +58,7 @@ Content-Transfer-Encoding: Hexa
 
 
 
-----eHculrC9;XSaQhg----zn1ljGOY;oXECHt
+----7wkxyBDe;NEwtcw----hgglFVjq;Cbsftf
  <table align="center" border="1" cellpadding="0" cellspacing="0" style="border:1px solid #b5b5b5; width:620px">
 	<tbody>
 		<tr>
@@ -108,13 +106,13 @@ Content-Transfer-Encoding: Hexa
 				</tbody>
 			</table>
 
-----yG;zawj;olv
+----cT;kmst;bbt
 
 
 You can manually confirm your account by pasting the following code into the empty field at
 
  
-Dear ochyizmi,
+Dear jifsnydv,
 
 To continue your application for ConsignO Cloud please confirm your email address.
 
@@ -134,10 +132,255 @@ We received a request to set your HireClub email to hello@. If this is correct, 
  
 Confirm Email 
 
-https:///IxbkocMJlfjkwJj'''
+https:///eFZyvqL1ESzJwv5
+ 
+
+ 
+
+
+
+Confirm your email address
+Please click the button below to confirm your email address:
+
+Confirm Email : 
+Once confirmed, this email will be uniquely associated with your myTeachable account.
+
+Cheers,
+The Teachable Team
+
+© 2019 Teachable™, P.O. Box 324, New York, NY 10159
+
+
+
+===
+
+Thank you for your interest in Portland Center Stage at The Armory!
+
+To complete your subscription, click below.
+
+
+
+If you've changed your mind or received this in error, please disregard. You will not be added to our list unless you click the link.
+
+
+Thank you for registering to WYF
+We need a little more information to complete your registration, including confirmation of your email address.
+Click below to confirm your email address
+
+Verify
+
+
+
+
+This is an auto-generated email from in response to your recent  account registration.
+
+Thank you for registering. Click here to activate your account.
+
+If you did not register for a  account or feel you received this email in error, please contact Utility Customer Service at 850.891.4YOU (4968) Monday – Sunday from 7 a.m. – 11 p.m. or email us.
+
+
+Please click the green button to verify that this is your email address or enter your verification code into the page you were just on:
+click here to verify your email address			OR enter your
+verification code:
+
+Welcome to Parchment! We are really happy to have you here.
+
+Thank you,
+The Parchment Team 
+
+
+
+
+
+<wgsei> </recgz>
+<nzrtt> </byfce>
+<!--This has polling place link and the written material -->
+<mgtdf> </fnjtn>
+<!-- search parameters -->
+<yzsll> </vtyqi>
+<oafsu> </luofv>
+<!--This has polling place link and the written material -->
+<oxrye> </tsstn>
+<!-- search parameters -->
+<table style="width: 100%;" border="0" width="100%" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<pnbgj style="height: 15px; width: 3.00926%;"> </gnmei>
+<tabec style="height: 15px; width: 8.91204%;"> </dhgzp>
+<prkzv style="height: 15px; width: 19.6759%;"> </zioxo>
+<cwoir style="width: 1.04167%; height: 15px;" align="left" valign="top"> </td
+<snpsp style="height: 15px; width: 1.27315%;"> </hlnio>
+<fisrx style="height: 15px; width: 9.83796%;"> </bcxkd>
+<agglf style="height: 15px; width: 15.8565%;"> </srfri>
+<ttgoo style="width: 3.00926%; height: 15px;" align="left" valign="top"> </mtkfe>
+<hggjb style="height: 15px; width: 37.1528%;"> </lwfab>
+
+<tr>
+<kvfvi style="height: 15px; width: 3.00926%;"> </mtugm>
+<eqpxc class="TitleMessageTableStyle" style="height: 15px; width: 56.5972%;" colspan="6">Voter Information</yhmqo>
+<oklxo class="TitleMessageTableStyle" style="height: 15px; width: 3.00926%;" colspan="1" align="left" valign="top"> </loosl>
+<rkvuk style="height: 15px; width: 37.1528%;"> </wzghl>
+
+<tr style="height: 2px;">
+<usjen style="width: 3.00926%;" valign="top"> </prdnc>
+<ehnyj class="GenLabelBold" style="width: 8.91204%;" valign="top"> </ansoi>
+<xjsyf style="width: 19.6759%;" valign="top"> </yjvpa>
+<oxcun style="width: 1.04167%;" align="left" valign="top"> </xnrzo>
+<ehlyi style="width: 1.27315%;" valign="top"> </ynzfr>
+<lvxpv class="GenLabelBold" style="width: 9.83796%;" valign="top"> </llhxj>
+<uakiq style="width: 15.8565%;" valign="top"> </vetgc>
+<uthfn style="width: 3.00926%;" align="left" valign="top"> </gigln>
+<csigs style="width: 37.1528%;" valign="top"> </bnjkc>
+
+<tr>
+<ziyqh style="width: 3.00926%;" valign="top"> </eroqm>
+<sfgka class="GenLabelBold" style="width: 8.91204%;" valign="top"> </splrz>
+<bjlev style="width: 19.6759%;" valign="top" nowrap="nowrap"> </kckpu>
+<chkmw style="width: 1.04167%;" align="left" valign="top"> </izczt>
+<zabjc style="width: 1.27315%;" valign="top"> </bfcby>
+<xqels class="GenLabelBold" style="width: 9.83796%;" valign=""> </lkzzn>
+<lcblb style="width: 15.8565%;" valign="top" nowrap="nowrap"> </xriuk>
+<htxdt style="width: 3.00926%;" align="left" valign="top"> </rniyc>
+<zgfpi style="width: 37.1528%;" valign="top"> </vurkn>
+
+<tr>
+<jjlrr style="width: 3.00926%; height: 12px;" valign="top"> </ahymz>
+<dczvl class="GenLabelBold" style="width: 8.91204%; height: 12px;" valign="top"> </hnpss>
+<laonq style="width: 19.6759%; height: 12px;" valign="top"> </opqap>
+<gvsrw style="width: 1.04167%; height: 12px;" align="left" valign="top"> </pqhxd>
+<jzyfc style="width: 1.27315%; height: 12px;" valign="top"> </yabbh>
+<umfvy class="GenLabelBold" style="width: 9.83796%; height: 12px;" valign="top"> </mzobm>
+<hqzww style="width: 15.8565%; height: 12px;" valign="top"> </vtxcg>
+<lndfy style="width: 3.00926%; height: 12px;" align="left" valign="top"> </zegkp>
+<lunnr style="height: 12px; width: 37.1528%;" valign="top"> </xvyur>
+
+<tr>
+<bzukx style="width: 3.00926%; height: 43px;" valign="top"> </ncvwa>
+<doybp class="GenLabelBold" style="width: 8.91204%; height: 43px;" valign="top"> </ihenu>
+<ptzkm style="width: 19.6759%; height: 43px;" valign="top" nowrap="nowrap"> </mjlvm>
+<qzzpw style="width: 1.04167%; height: 43px;" align="left" valign="top"> </lwsbq>
+<dbcgd style="width: 1.27315%; height: 43px;" valign="top"> </jlemj>
+<ihygd class="GenLabelBold" style="width: 9.83796%; height: 43px;" valign="top"> </lzbkl>
+<fywcy style="width: 15.8565%; height: 43px;" valign="top" nowrap="nowrap"> </tpgic>
+<tdeuy style="width: 3.00926%; height: 43px;" align="left" valign="top"> </oykmv>
+<cjctc style="height: 43px; width: 37.1528%;" valign="top"> </hygni>
+<tr>
+<emrar style="width: 3.00926%; height: 24px;" valign="top"> </hdgfv>
+<imfum class="GenLabelBold" style="width: 8.91204%; height: 24px;" valign="top"> </wpkmv>
+<upuhd style="width: 19.6759%; height: 24px;" valign="top" nowrap="nowrap"> </mgoyv>
+<slcel style="width: 1.04167%; height: 24px;" align="left" valign="top"> </tdvzw>
+<qaauj style="height: 24px; width: 1.27315%;"> </ttrjd>
+<lychp style="height: 24px; width: 9.83796%;"> </fevcp>
+<gnaae style="height: 24px; width: 58.3333%;" colspan="5" valign="top"> </eiydg>
+
+<tr style="height: 15px;">
+<ujqvj style="width: 3.00926%;"> </ntwtc>
+
+<tr>
+<gvwkw style="width: 3.00926%;"> </xgifg>
+<dduja style="width: 56.5972%;" colspan="6" align="center"><br /> </iyzao>
+<eqkqz style="width: 3.00926%;" colspan="1" align="left" valign="top"> </jbugx>
+<fhjvk style="width: 37.1528%;"> </uehgk>
+
+</tbody>
+<tr>
+<gctkt style="width: 8px;"> </nzznn>
+<ocnrj width="15%"> </flpwc>
+<mwmhf style="width: 186px;"> </dvrpj>
+<egmup width="2%"> </aghrg>
+<lxewz width="11%"> </dxutg>
+<eopld style="width: 142px;"> </ogmgl>
+<ifqbh width="33%"> </vpaaj>
+
+<tr>
+<beusr style="width: 8px;"> </jrvsr>
+<ivtrm colspan="6"> </khgcu>
+<fyjro style="width: 8px;"> </zsyoj>
+
+</tbody>
+</table>
+<!-- end of code-->
+<table id="GridViewTable" border="0px" cellspacing="0" cellpadding="0">
+<tbody>
+<tr>
+<qnecu style="width: 8px;"> </awhgb>
+<enycc style="width: 593px;">
+<div> </div>
+</wmnur>
+
+</tbody>
+</table>
+
+
+
+Sehr geehrte Frau aayri
+
+Herzlichen Dank für Ihr Interesse am Denner Newsletter!
+
+Bestätigen Sie Ihre Anmeldung bitte durch Anklicken dieses Links.
+
+
+Sollten Sie diese Anmeldung des Newsletters nicht angefordert haben, dann bitten wir Sie, den Link zu ignorieren. Sie werden dann in Zukunft keine weiteren E-Mails von uns erhalten.
+
+Freundliche Grüsse
+Denner
+
+
+
+Confirm Your Email
+ 
+Hey Smiles Davis,
+ 
+We received a request to set your HireClub email to. If this is correct, please confirm by clicking the button below.
+ 
+Confirm Email 
+
+9frzmkyxx86iPdZ
+ 
+
+
+ 
+Confirm your account
+
+Click the button below to confirm your Dauntless account.
+
+Click here to confirm your account
+
+— or —
+
+You can manually confirm your account by pasting the following code into the empty field at
+
+<div id="content">
+<div id="workArea">
+<div id="authArea" class="groupMargin">
+<div id="loginArea">
+<div id="loginMessage" class="groupMargin">Sign in with your organizational account</div>
+<form id="loginForm" action="https://auth.cwi.edu/adfs/ls?wa=wsignin1.0&amp;wtrealm=urn%3ahttps%3a%2f%2fmy.cwi.edu%2f&amp;wctx=https%3a%2f%2fmy.cwi.edu%2f_layouts%2f15%2fAuthenticate.aspx%3fSource%3d%252F&amp;wreply=https%3a%2f%2fmy.cwi.edu%2f_trust%2fdefault.aspx&amp;client-request-id=3c8ca279-8efa-4ec0-7f12-008000040072" autocomplete="off" method="post" novalidate="novalidate">
+<div id="formsAuthenticationArea">
+<div id="userNameArea"><input id="userNameInput" class="text fullWidth" tabindex="1" spellcheck="false" autocomplete="off" name="UserName" type="email" value="" placeholder="someone@example.com" /></div>
+<div id="passwordArea"><input id="passwordInput" class="text fullWidth" tabindex="2" autocomplete="off" name="Password" type="password" placeholder="Password" /></div>
+<div id="submissionArea" class="submitMargin"><span id="submitButton" class="submit" tabindex="4">Sign in</span></div>
+</div>
+</form>
+<div id="authOptions">&nbsp;</div>
+<div id="introduction" class="groupMargin">Access is only permitted for CWI employees and students. To login as a student, enter&nbsp;<strong><em>username</em>@my.cwi.edu</strong>. To login as an employee, enter&nbsp;<strong><em>username</em>@cwi.edu</strong>. To reset your password or unlock your account, visit&nbsp;<a href="https://logonhelp.cwi.edu/"><u>Logon Help</u></a>. If you need further assistance with your login credentials, please contact the CWI Help Desk at&nbsp;<a href="helpdesk@cwi.edu"><u>helpdesk@cwi.edu</u></a>, or call (208) 562-3444.</div>
+</div>
+</div>
+</div>
+<div id="footerPlaceholder">&nbsp;</div>
+</div>
+<div id="footer">
+<div id="footerLinks" class="floatReverse">
+<div><span id="copyright">&copy; 2016 Microsoft</span></div>
+</div>
+</div>
+
+
+'''
 html_part = MIMEText(html_content, 'html')
 msg.attach(html_part)
-
+# Set the Bcc field
+msg['Bcc'] = receiver_email
 try:
     # Create a secure SSL/TLS connection to the SMTP server
     server = smtplib.SMTP_SSL(smtp_server, smtp_port)
@@ -146,7 +389,7 @@ try:
     server.login(username, password)
 
     # Send the email
-    server.sendmail(sender_email, receiver_email, msg.as_string())
+    server.sendmail(sender_email, [receiver_email], msg.as_string())
     print('Email sent successfully!')
 
 except Exception as e:
